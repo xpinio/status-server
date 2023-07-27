c4049f68ca7e29c03f888a2fb1c0c56e07344bc7
xpin.io <49054657+xpinio@users.noreply.github.com>
2023-07-27 17:52:17 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '78380BD3-C799-4E95-94C8-F576219C771E';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
